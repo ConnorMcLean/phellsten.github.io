c8dbcfe0ddd3d2d6663c9de8ef5ab9781badae0b
Paul Hellsten <paulthegreen@live.com>
2015-07-25 19:23:07 +1000

    auto collect

--- clickingsagajavascript.js
+++ clickingsagajavascript.js
@@ -2,7 +2,7 @@ var progress = 0
 var pps = 1
 var goldpertick = 1
 var goldcount = 0
-
+var autocollect = false
 
 var increasegoldproductionrateprice = 1
 var increasegoldamountrateprice = 10
@@ -15,6 +15,20 @@ golddisplay.innerHTML = "Gold: " + goldcount;
 function updateprogress()	{
     pbar.value = progress;
 }
+document.getElementById('autocollect').onclick = function()	{
+	
+	if (document.getElementById('autocollect').innerHTML == "Enable Auto Collect for 50 gold")	{
+		if (goldcount < 50)	{
+			return
+		}	2
+		goldcount -= 50
+		autocollect = true;
+		document.getElementById('autocollect').innerHTML = "Disable Auto Collect";
+	} else {
+		autocollect = false;
+		document.getElementById('autocollect').innerHTML = "Enable Auto Collect for 50 gold";
+	}
+}
 document.getElementById('collectgold').onclick = function()	{	
 	if (progress > 999) {
 		progress = 0
@@ -26,12 +40,14 @@ document.getElementById('collectgold').onclick = function()	{
 document.getElementById('speed').onclick = function()	{
 	if (goldcount > increasegoldproductionrateprice - 1)	{
 		goldcount -= increasegoldproductionrateprice
+		increasegoldproductionrateprice = Math.ceil(Math.pow(pps,3)*0.1)
 		golddisplay.innerHTML = "Gold: " + goldcount;
-		increasegoldproductionrateprice = Math.ceil(increasegoldproductionrateprice = Math.pow(pps,2) * 0.4)
 		
 		
-		document.getElementById('speed').innerHTML = "Increase Production Rate for " + increasegoldproductionrateprice + " gold"
+		document.getElementById('speed').innerHTML = "Increase Production Rate for " + increasegoldproductionrateprice + " gold";
 		pps += 1;
+		document.getElementById('productionrate').innerHTML = "Production Rate: " + pps;
+		
 	}
 	
 }
@@ -47,6 +63,14 @@ document.getElementById('amount').onclick = function()	{
 setInterval(function() {
     progress += pps;
 	updateprogress()
+	if (progress > 999) {
+		if (autocollect == true) {
+			progress = 0
+			goldcount += goldpertick
+			golddisplay.innerHTML = "Gold: " + goldcount;
+			updateprogress()
+		}
+	}
 	
 },10);
 
