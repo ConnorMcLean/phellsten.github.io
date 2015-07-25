7daf510f2c2e536600b92a5a90034c498e37ba20
Paul Hellsten <paulthegreen@live.com>
2015-07-25 23:44:59 +1000

    changed stuff

--- clickingsagajavascript.js
+++ clickingsagajavascript.js
@@ -1,9 +1,10 @@
 var progress = 0
 var pps = 1
+var ppstracker = 1
 var goldpertick = 1
-var goldcount = 0
+var goldcount = 2
 var autocollect = false
-
+var souls = 0
 var increasegoldproductionrateprice = 1
 var increasegoldamountrateprice = 10
 var pbar = document.getElementById("progressbar");
@@ -40,14 +41,22 @@ document.getElementById('collectgold').onclick = function()	{
 document.getElementById('speed').onclick = function()	{
 	if (goldcount > increasegoldproductionrateprice - 1)	{
 		goldcount -= increasegoldproductionrateprice
-		increasegoldproductionrateprice = Math.ceil(Math.pow(pps,3)*0.1)
+		
+		pps += 1;
+		ppstracker += 1;
+		if (pps == 101){
+			pps = 1
+			goldpertick = goldpertick * 100
+			souls += 1
+			document.getElementById('goldpertick').innerHTML = "Gold Per Tick: " + goldpertick
+			document.getElementById('souls').innerHTML = "Souls: " + souls
+		}
+		document.getElementById('productionrate').innerHTML = "Production Rate: " + ppstracker;
+		increasegoldproductionrateprice = Math.ceil(Math.pow(ppstracker,3)*0.1*(souls + 1))
 		golddisplay.innerHTML = "Gold: " + goldcount;
 		
 		
 		document.getElementById('speed').innerHTML = "Increase Production Rate for " + increasegoldproductionrateprice + " gold";
-		pps += 1;
-		document.getElementById('productionrate').innerHTML = "Production Rate: " + pps;
-		
 	}
 	
 }
@@ -55,8 +64,9 @@ document.getElementById('amount').onclick = function()	{
 	if (goldcount > increasegoldamountrateprice - 1)	{
 		goldcount -= increasegoldamountrateprice
 		golddisplay.innerHTML = "Gold: " + goldcount;
-		increasegoldamountrateprice += increasegoldamountrateprice * 2
+		increasegoldamountrateprice += increasegoldamountrateprice * (2 + souls)
 		goldpertick = goldpertick * 2
+		document.getElementById('goldpertick').innerHTML = "Gold Per Tick: " + goldpertick
 		document.getElementById('amount').innerHTML = "Increase Amount Found for " + increasegoldamountrateprice + " gold";
 	}
 }
